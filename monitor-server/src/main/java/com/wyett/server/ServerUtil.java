5de340a75147a1040b2b67d9ca590540647f2655
wyettlei@hotmail.com <wyettlei@hotmail.com>
2019-12-30 20:08:36 +0800

    server point heartbeat

---
+++ monitor-server/src/main/java/com/wyett/server/ServerUtil.java
@@ -0,0 +1,21 @@
+package com.wyett.server;
+
+import org.I0Itec.zkclient.util.ZkPathUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author : wyettLei
+ * @date : Created in 2019/12/30 16:51
+ * @description: TODO
+ */
+
+public class ServerUtil {
+    private static final Logger LOG = LoggerFactory.getLogger(ServerUtil.class);
+
+    public ServerUtil() {
+
+    }
+
+
+}
