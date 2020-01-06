50c324188b84803873777a7ad666ce17225f7112
wyettlei@hotmail.com <wyettlei@hotmail.com>
2020-01-06 19:30:45 +0800

    read config file

---
+++ monitor-common/src/main/java/com/wyett/common/config/Resource.java
@@ -0,0 +1,43 @@
+package com.wyett.common.config;
+
+import com.wyett.common.util.MonitorConfigUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Enumeration;
+
+/**
+ * @author : wyettLei
+ * @date : Created in 2020/1/6 19:22
+ * @description: TODO
+ */
+
+public class Resource {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Resource.class);
+
+    private static InputStream getResource(String name) {
+        InputStream in = null;
+        String osName = System.getProperties().getProperty("os.name");
+        try {
+            if (osName.contains("Win") || osName.contains("Mac")) {
+                in = Thread.currentThread().getContextClassLoader().getResourceAsStream(name);
+            } else if(osName.contains("Linux")) {
+                in = new FileInputStream(System.getProperties().getProperty("user.dir") + "/" + name);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                in.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        LOG.debug("config loaded sucessfully...");
+        return in;
+    }
+}
